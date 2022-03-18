89ce56f9bc4e61e857bba0adb38040452db4a378
Shammo2 <samhammond2017@gmail.com>
2022-03-18 07:16:31 -0700

    linked

--- linkedlist.cs
+++ linkedlist.cs
@@ -1,12 +1,51 @@
-
+using System;
+using System.Collections.Generic;
+using System.Collections;
 using UI;
 
 public class linked{
+	int number;
+    Boolean isEval;
+    linked next;
+    public linked(){
+        number = 0;
+        next = null;
+        isEval = false;
+    }
+    linked(int val, linked n){
+        number = val;
+        next = n;
+        isEval = false;
+    }
+	
+	static linked findsame(linked A, linked B){
+            while(A.next != null){
+                A.isEval = true;
+                A = A.next;
+            }
+            
+            while(B.next != null){
+                if (B.isEval == true)
+                {return B;}
+                B.isEval = true;
+                B = B.next;
+            }
+			
+            linked notNode = new linked();
+			return notNode;
 
-    public void linkedlists(){
+}
+public void linkedlists(){
 
-
-
-        Console.WriteLine("it worked bruh linked list time bby ");
     }
 }
+
+
+
+/*
+ Given two singly linked lists that intersect at some point, find the intersecting node.
+ Assume the lists are non-cyclical.
+For example, given A = 3 ➔ 7 ➔ 8 ➔ 10 and B = 99 ➔ 1 ➔ 8 ➔ 10, return the node with number 8.
+In this example, assume nodes with the same value are the exact same node objects.
+Do this in 0( m + n) time (where m and n are the lengths of the lists) and constant space.
+*/
--- stack.cs
+++ stack.cs
@@ -1,33 +1,51 @@
 using System.Collections.Generic;
+using System.Collections;
 using UI;
+using System;
 public class stack{
 
 public void stackstart(){
+    
+    Stack newStack = new Stack();
+    bool exit = false; 
+    while(!exit){
+        Console.WriteLine("[1] push to stack");
+        Console.WriteLine("[2] pop from stack");
+        Console.WriteLine("[3] push to stack");
+        Console.WriteLine("[x] exit");
+        string input = Console.ReadLine();
+        switch(input){
+            case "1":
+            Console.WriteLine("you chose: push to stack");
+            Console.WriteLine("enter a number");
+            string stackstring = Console.ReadLine();
+            int stacknumber = Int32.Parse(input);
+            newStack.Push(stacknumber);
+            break; 
+            case "2":
+            Console.WriteLine("you chose: pop from stack");
+            Console.WriteLine( "(Peek)\t\t{0}", newStack.Peek() );
+            break; 
+            case "3":
+            Console.WriteLine("you chose: push to stack");
+            break; 
+            case "x":
+            exit = true;
+            break;
 
-    List<int> stack = new List<int>();
-    Console.WriteLine("[1] push to stack");
-    Console.WriteLine("[2] pop from stack");
-    Console.WriteLine("[3] push to stack");
-    string input = Console.ReadLine();
-    switch(input){
-        case "1":
-        Console.WriteLine("you chose: push to stack");
-        break; 
-        case "2":
-        Console.WriteLine("you chose: pop from stack");
-        break; 
-        case "3":
-        Console.WriteLine("you chose: push to stack");
-        break; 
-
-
+        }
     }
-
-
+}
 
 }
 
 
-
-
-}
+/*
+Implement a stack that has the following methods:
+• push ( v a 1 ) : push v a 1 onto the stack
+• pop: pop off and return the topmost element of the stack.
+If there are no elements in the stack, throw an error.
+• max: return the maximum value in the stack currently.
+If there are no elements in the stack, throw an error.
+Each method should run in constant time.
+*/
