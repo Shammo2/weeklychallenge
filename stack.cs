fbc9e24fa08cdd4e2fbdfff29490218ef4dbfcb1
Shammo2 <samhammond2017@gmail.com>
2022-03-18 06:45:08 -0700

    stack

--- stack.cs
+++ stack.cs
@@ -1,9 +1,30 @@
-
+using System.Collections.Generic;
 using UI;
 public class stack{
 
 public void stackstart(){
-Console.WriteLine("it works stack time bby");
+
+    List<int> stack = new List<int>();
+    Console.WriteLine("[1] push to stack");
+    Console.WriteLine("[2] pop from stack");
+    Console.WriteLine("[3] push to stack");
+    string input = Console.ReadLine();
+    switch(input){
+        case "1":
+        Console.WriteLine("you chose: push to stack");
+        break; 
+        case "2":
+        Console.WriteLine("you chose: pop from stack");
+        break; 
+        case "3":
+        Console.WriteLine("you chose: push to stack");
+        break; 
+
+
+    }
+
+
+
 }
 
 
